c9908a55381b7b9fd2f3dbca6bb36455d27b8b71
coolbuddy2991 <saurabhkatti2991@gmail.com>
2024-12-25 18:49:35 +0530

    added dockerfile

--- app.py
+++ app.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route("/about")
+def about():
+    
+    return "This is about page"
 
 if __name__ == "__main__":
     app.run(debug=True)
